25d3b1ca50c3a860a6f57c5f430fe17b7e3e5ec1
Athos Benther <athum@live.com>
2021-03-09 07:14:02 -0300

    Makes light stronger, white, and narrower

--- anpeq/scripts/anpeqLight.c
+++ anpeq/scripts/anpeqLight.c
@@ -1,18 +1,18 @@
 class anpeqLight extends SpotLightBase
 {
-	private static float m_DefaultBrightness = 9;
-	private static float m_DefaultRadius = 40;
+	private static float m_DefaultBrightness = 10;
+	private static float m_DefaultRadius = 60;
 	
 	void anpeqLight()
 	{
 		SetVisibleDuringDaylight(true);
 		SetRadiusTo(m_DefaultRadius);
-		SetSpotLightAngle(50);
+		SetSpotLightAngle(45);
 		SetCastShadow(true);
 		SetBrightnessTo(m_DefaultBrightness);
 		SetFadeOutTime(0.05);
-		SetAmbientColor(1.0, 0.9, 0.8);
-		SetDiffuseColor(1.0, 0.9, 0.8);
+		SetAmbientColor(1, 1, 1);
+		SetDiffuseColor(1, 1, 1);
 	}
 	
 	void SetIntensity( float coef, float time )
